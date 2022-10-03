daec14e7eecd0ed27c5054f7f73b628775685a6c
Hugo Salazar <hugo.salazar@soft-impact.com>
2022-10-03 10:55:40 +0100

    GenProto++

--- proto/go_gen.sh
+++ proto/go_gen.sh
@@ -1,7 +1,9 @@
 export PATH="$PATH:$(go env GOPATH)/bin"
 
+cp cheers ../genproto -r
+
 protoc --proto_path=. \
   --go_out=../genproto --go_opt=paths=source_relative  \
   --go-grpc_out=../genproto --go-grpc_opt=paths=source_relative \
   --experimental_allow_proto3_optional \
-    cheers/api/v1/main.proto cheers/type/party.proto
+    cheers/api/v1/main.proto cheers/type/party/party.proto cheers/type/privacy/privacy.proto
