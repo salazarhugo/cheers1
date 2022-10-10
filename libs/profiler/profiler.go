eeb95eaebba2ae9bf29b47380de4583f09f17a74
Hugo Salazar <hugo.salazar@soft-impact.com>
2022-10-10 16:12:22 +0100

    Added google cloud profiler

---
+++ libs/profiler/profiler.go
@@ -0,0 +1,27 @@
+package profiler
+
+import (
+	"cloud.google.com/go/profiler"
+	"github.com/labstack/gommon/log"
+	"time"
+)
+
+func initProfiling(service, version string) {
+	for i := 1; i <= 3; i++ {
+		if err := profiler.Start(profiler.Config{
+			Service:        service,
+			ServiceVersion: version,
+			// ProjectID must be set if not running on GCP.
+			// ProjectID: "my-project",
+		}); err != nil {
+			log.Warnf("failed to start profiler: %+v", err)
+		} else {
+			log.Info("started Stackdriver profiler")
+			return
+		}
+		d := time.Second * 10 * time.Duration(i)
+		log.Infof("sleeping %v to retry initializing Stackdriver profiler", d)
+		time.Sleep(d)
+	}
+	log.Warn("could not initialize Stackdriver profiler after retrying, giving up")
+}
