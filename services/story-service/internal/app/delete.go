d6f9d0a8828c74eaf155f0cef01eb444b947500d
Hugo Salazar <hugo.salazar@soft-impact.com>
2022-10-24 16:04:42 +0100

    Implement delete.go

--- services/story-service/internal/app/delete.go
+++ services/story-service/internal/app/delete.go
@@ -3,6 +3,9 @@ package app
 import (
 	"context"
 	pb "github.com/salazarhugo/cheers1/genproto/cheers/story/v1"
+	"github.com/salazarhugo/cheers1/libs/auth/utils"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -10,6 +13,20 @@ func (s *Server) DeleteStory(
 	ctx context.Context,
 	request *pb.DeleteStoryRequest,
 ) (*emptypb.Empty, error) {
-	//TODO implement me
-	panic("implement me")
+	_, err := utils.GetUserId(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed retrieving userID")
+	}
+
+	storyID := request.GetId()
+	if storyID == "" {
+		return nil, status.Error(codes.InvalidArgument, "id parameter can't be blank")
+	}
+
+	err = s.storyRepository.DeleteStory(storyID)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to delete story")
+	}
+
+	return &emptypb.Empty{}, err
 }
